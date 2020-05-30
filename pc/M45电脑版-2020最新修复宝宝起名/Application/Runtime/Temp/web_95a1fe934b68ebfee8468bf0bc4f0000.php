fc109d6c62f31c7e964f9c36b5d0d69d12d99c99
赵思贵 <1060656096@qq.com>
2020-05-30 13:43:56 +0800

    ddd: 删除多于文件夹

--- pc/M45电脑版-2020最新修复宝宝起名/Application/Runtime/Temp/web_95a1fe934b68ebfee8468bf0bc4f0000.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:17:{s:6:"weburl";s:19:"http://www.177.name";s:8:"webtitle";s:52:"易起起名网_中国权威易经起名策划机构";s:8:"webphone";s:11:"18818881888";s:5:"webqq";s:8:"88888888";s:9:"webpccopy";s:54:"版权所有 Copyright  2010-2019【易起起名网】";s:5:"price";s:3:"0.1";s:9:"old_price";s:2:"88";s:8:"wx_appid";s:18:"wxc4ebd47b704cd18e";s:8:"wx_mchid";s:10:"1527013011";s:6:"wx_key";s:32:"7a03f0e72b796de1099a974ed1f25c53";s:12:"wx_appsecert";s:32:"7a03f0e72b796de1099a974ed1f25c53";s:8:"__hash__";s:65:"8ceec297fd4be151008bd6ebab8e7576_7839cbeb6bd698dbd146ea2c2ca3434d";s:11:"webkeywords";s:9:"起名网";s:14:"webdescription";s:9:"起名网";s:11:"ali_partner";s:16:"2088002193121922";s:16:"ali_seller_email";s:12:"ioni@ioni.cn";s:7:"ali_key";s:32:"buvhyhzdsfn213j4lmat7n4clsr0xp4c";}
-?>
--- pc/M45电脑版-2020最新修复宝宝起名/Public/Home/js/rightfix.js
+++
@@ -1,8 +0,0 @@
-document.writeln('<style>');
-document.writeln('.fix_rightx { width: 125px;position: fixed;top: 63% ;right:0; text-align: center;background:#E44D4D }');
-document.writeln('.fix_rightx .a{background: #E44D4D;height: 45px;width: 125px;display: block;color: #fff;line-height: 45px;font-size: 16px;}');
-document.writeln('</style>');
-document.writeln('<div class="fix_rightx" >');
-document.writeln('<a href="" class="a" style="height:70px;text-align:center;line-height:30px">客服电话<br>13023193439');
-document.writeln('<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=3477281956&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:3477281956:53" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>');
-document.writeln('</a></div>');
